add3ecf74d8854f0ddf9ab43053efc35c9ed6068
Julian Brecht <julian.brecht@glauxgroup.ch>
2024-03-21 14:00:12 +0100

    Added functions to delete and add data

---
+++ readme.md
@@ -0,0 +1,5 @@
+Um diese App zu nutzen, muss in der config.json Datei im SRC Ordner die prop apiWeatherKey mit dem API Key befüllt werden:
+
+"apiWeatherKey": "YourKeyHere"
+
+"YourKeyHere" muss mit dem persönlichen API-Schlüssel von OpenWeatherMap ersetzt werden.
--- src/index.js
+++ src/index.js
@@ -3,44 +3,109 @@ import { h, diff, patch } from "virtual-dom";
 import createElement from "virtual-dom/create-element";
 
 // allows using html tags as functions in javascript
-const { div, button, p, h1 } = hh(h);
+const { div, button, p, h1, input, label, table, th, td, tr, thead, tbody } =
+  hh(h);
 
 // A combination of Tailwind classes which represent a (more or less nice) button style
-const btnStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+const btnStyle =
+  "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
 
 // Messages which can be used to update the model
 const MSGS = {
   UPDATE_MODEL: "UPDATE_MODEL",
-  UPDATE_RANDOM_NUMBER: "UPDATE_RANDOM_NUMBER",
-  TEST: "TEST"
+  ADD_ITEM: "ADD_ITEM",
+  DELETE_ITEM: "DELETE_ITEM",
   // ... ℹ️ additional messages
 };
 
 // View function which represents the UI as HTML-tag functions
 function view(dispatch, model) {
   return div({ className: "flex flex-col gap-4 items-center" }, [
-    h1({ className: "text-2xl" }, `My Title`),
-    button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_MODEL) }, "Update Model"),
-    p({ className: "text-2xl" }, `Time: ${model.currentTime}`),
-    button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_RANDOM_NUMBER) }, "Update Random Number"),
-    p({ className: "text-2xl" }, `Random Number: ${model.randomNumber}`),
-    // ... ℹ️ additional elements
+    h1({ className: "text-2xl" }, `Weather`),
+    div({ className: "flex " }, [
+      label({ for: "input1", type: "text" }, "Location"),
+      input({ className: "ml-2 shadow-md border w-96 mr-5", id: "input1" }),
+      button(
+        {
+          className: btnStyle,
+          onclick: () =>
+            dispatch({
+              type: MSGS.ADD_ITEM,
+              payload: {
+                location: document.getElementById("input1").value,
+              },
+            }),
+        },
+        "Add"
+      ),
+    ]),
+    div({ className: "flex " }, [
+      table([
+        thead([
+          tr({ className: "p-4" }, [
+            th({ className: "p-4" }, "Location"),
+            th({ className: "p-4" }, "Temp"),
+            th({ className: "p-4" }, "High"),
+            th({ className: "p-4" }, "Low"),
+            th({ className: "p-4" }, "Action"),
+          ]),
+        ]),
+        tbody(
+          { id: "tbody" },
+          model.tItems.map((item) =>
+            tr([
+              td({ className: "p-2 text-center" }, item.location),
+              td({ className: "p-2 text-center" }, item.temp.toString()),
+              td({ className: "p-2 text-center" }, item.high.toString()),
+              td({ className: "p-2 text-center" }, item.low.toString()),
+              td([
+                button(
+                  {
+                    className: btnStyle,
+                    onclick: () =>
+                      dispatch({ type: MSGS.DELETE_ITEM, payload: item.id }),
+                  },
+                  "Delete"
+                ),
+              ]),
+            ])
+          )
+        ),
+      ]),
+    ]),
   ]);
 }
 
 // Update function which takes a message and a model and returns a new/updated model
 function update(msg, model) {
-  switch (msg) {
-    case MSGS.UPDATE_MODEL:
-      return { ...model, currentTime: new Date().toLocaleTimeString() };
+  switch (
+    msg.type // Verwende msg.type, um den Nachrichtentyp zu prüfen
+  ) {
+    case MSGS.DELETE_ITEM:
+      const iddel = msg.payload; // Verwende msg.payload, um auf die Daten zuzugreifen
+      const filteredTItems = model.tItems.filter((item) => item.id !== iddel);
+      return { ...model, tItems: filteredTItems };
+    case MSGS.ADD_ITEM:
+      let request = new XMLHttpRequest();
+      request.open("GET", "http://192.168.0.196:9000/config.json", false); 
+      request.send(null);
+      if (request.status === 200) {
+        let configData = JSON.parse(request.responseText);
+        console.log(configData.apiWeatherKey);
+      }
+      const location = msg.payload.location;
+      const temp = 1;
+      const high = 2;
+      const low = 1;
+      const id = Date.now();
+      const newTItem = { id, location, temp, high, low };
+      const updatedTItems = model.tItems.concat(newTItem);
+      return { ...model, tItems: updatedTItems };
 
-    case MSGS.UPDATE_RANDOM_NUMBER:
-      return { ...model, randomNumber: Math.random() };
     default:
       return model;
   }
 }
-
 // ⚠️ Impure code below (not avoidable but controllable)
 function app(initModel, update, view, node) {
   let model = initModel;
@@ -58,8 +123,15 @@ function app(initModel, update, view, node) {
 
 // The initial model when the app starts
 const initModel = {
-  currentTime: new Date().toLocaleTimeString(),
-  randomNumber: 1,
+  tItems: [
+    {
+      id: 1,
+      location: "blin",
+      temp: 1,
+      high: 2,
+      low: 1,
+    },
+  ],
 };
 
 // The root node of the app (the div with id="app" in index.html)
